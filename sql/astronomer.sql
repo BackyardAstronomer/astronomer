74aa6a69b198047824db6a9a612a0f7f02d066c5
Jack Jain <cirocelot@gmail.com>
2018-11-02 11:01:33 -0600

    added 'profile' create table statement

--- sql/astronomer.sql
+++ sql/astronomer.sql
@@ -6,4 +6,16 @@ DROP TABLE IF EXISTS comment
 DROP TABLE IF EXISTS event
 DROP TABLE IF EXISTS profile
 
-
+CREATE TABLE profile (
+	profileId BINARY(16) NOT NULL,
+	profileEmail VARCHAR(32) NOT NULL,
+	profileBio VARCHAR(255),
+	profileName VARCHAR(32) NOT NULL,
+	profileImage VARBINARY(64),
+	profileActivationToken VARCHAR(128) NOT NULL,
+	profileHash BINARY(64) NOT NULL,
+	UNIQUE (profileId),
+	UNIQUE (profileEmail),
+	UNIQUE (profileActivationToken),
+	PRIMARY KEY(profileId)
+);
