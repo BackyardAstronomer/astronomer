fef93f4056eb6eb867a7cea184c2a6fd31cd21af
ChamisaE <43251409+ChamisaE@users.noreply.github.com>
2018-11-04 15:23:52 -0700

    Completed types and comments for Event table.

--- sql/astronomer.sql
+++ sql/astronomer.sql
@@ -1,3 +1,4 @@
+-- The statement below sets the collation of the database to utf8
 ALTER DATABASE - CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 
 DROP TABLE IF EXISTS rsvp
@@ -22,13 +23,25 @@ CREATE TABLE profile (
 
 CREATE TABLE event (
 eventId BINARY(16) NOT NULL, --creates an unique 32 digit id for each event that is created, cannot be blank
-eventProfileId BINARY(16) NOT NULL, --
-eventEventTypeId BINARY(16) NOT NULL,
-eventContent VARCHAR(255) NOT NULL,
-eventTitle VARCHAR(32) NOT NULL,
-eventCounter
-eventStartDate DATETIME(6) NOT NULL,
-eventEndDate DATETIME(6) NOT NULL
+eventProfileId BINARY(16) NOT NULL, --creates a unique 32 digit id that associates profile to event, cannot be blank
+eventEventTypeId BINARY(16) NOT NULL, --creates a unique 32 digit id that associates the event to an event type, not blank
+eventContent VARCHAR(255) NOT NULL, --allows user to write a 255 character content blob detailing event, cannot be blank
+eventTitle VARCHAR(32) NOT NULL, --allows user to write short 32 character title for their event, cannot be blank
+eventCounter BINARY(16), --not positive if this is the correct var type for a counter..
+eventStartDate DATETIME(6) NOT NULL, --mm/dd/yy format, cannot be blank
+eventEndDate DATETIME(6) NOT NULL, --mm/dd/yy, cannot be blank
+--the following makes sure duplicate data cannot exist.
+UNIQUE(eventId),
+UNIQUE(eventProfileId),
+UNIQUE(eventEventTypeId),
+--the following establishes an index before making a foreign key.
+INDEX(eventProfileId),
+INDEX(eventEventTypeId),
+--the following creates the foreign key relationship
+FOREIGN KEY(eventProfileId) REFERENCES profile(profileId),
+FOREIGN KEY(eventEventTypeId) REFERENCES eventType(eventTypeId),
+--the following establishes the primary key for this table
+PRIMARY KEY(eventId)
 );
 /**
 This will be the third table. Chamisa, you'll want to insert the "event" table above this one
