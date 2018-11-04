e64a3a87ad0a1f5d767d0bce23f8835290fdab5f
ChamisaE <43251409+ChamisaE@users.noreply.github.com>
2018-11-04 15:28:04 -0700

    Added fields for everyone to insert comments.

--- sql/astronomer.sql
+++ sql/astronomer.sql
@@ -1,26 +1,31 @@
 -- The statement below sets the collation of the database to utf8
 ALTER DATABASE - CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 
+--dropping tables to run again with fresh inputs, eliminates run errors
 DROP TABLE IF EXISTS rsvp
 DROP TABLE IF EXISTS eventType
 DROP TABLE IF EXISTS comment
 DROP TABLE IF EXISTS event
 DROP TABLE IF EXISTS profile
 
+--
 CREATE TABLE profile (
-	profileId BINARY(16) NOT NULL,
-	profileEmail VARCHAR(32) NOT NULL,
-	profileBio VARCHAR(255),
-	profileName VARCHAR(32) NOT NULL,
-	profileImage VARBINARY(64),
-	profileActivationToken VARCHAR(128) NOT NULL,
-	profileHash BINARY(64) NOT NULL,
-	UNIQUE (profileId),
-	UNIQUE (profileEmail),
-	UNIQUE (profileActivationToken),
-	PRIMARY KEY(profileId)
+	profileId BINARY(16) NOT NULL, --
+	profileEmail VARCHAR(32) NOT NULL, --
+	profileBio VARCHAR(255), --
+	profileName VARCHAR(32) NOT NULL, --
+	profileImage VARBINARY(64), --
+	profileActivationToken VARCHAR(128) NOT NULL, --
+	profileHash BINARY(64) NOT NULL, --
+--
+	UNIQUE (profileId), --
+	UNIQUE (profileEmail), --
+	UNIQUE (profileActivationToken), --
+--
+	PRIMARY KEY(profileId) --
 );
 
+--This table establishes the variables for events and event creation.
 CREATE TABLE event (
 eventId BINARY(16) NOT NULL, --creates an unique 32 digit id for each event that is created, cannot be blank
 eventProfileId BINARY(16) NOT NULL, --creates a unique 32 digit id that associates profile to event, cannot be blank
@@ -43,18 +48,24 @@ FOREIGN KEY(eventEventTypeId) REFERENCES eventType(eventTypeId),
 --the following establishes the primary key for this table
 PRIMARY KEY(eventId)
 );
-/**
-This will be the third table. Chamisa, you'll want to insert the "event" table above this one
- */
+
+--
 CREATE TABLE comment (
-commentId BINARY(16) NOT NULL,
-commentEventId BINARY(16) NOT NULL,
-commentProfileId BINARY(16) NOT NULL,
-commentDate DATETIME(6) NOT NULL,
-commentContent VARCHAR (255) NOT NULL,
-INDEX (commentEventId),
-INDEX (commentProfileId),
-FOREIGN KEY (commentEventId) REFERENCES event(eventId),
-FOREIGN KEY (commentProfileId) REFERENCES profile(profileId),
-PRIMARY KEY (commentId)
+commentId BINARY(16) NOT NULL, --
+commentEventId BINARY(16) NOT NULL, --
+commentProfileId BINARY(16) NOT NULL, --
+commentDate DATETIME(6) NOT NULL, --
+commentContent VARCHAR (255) NOT NULL, --
+INDEX (commentEventId), --
+INDEX (commentProfileId), --
+--
+FOREIGN KEY (commentEventId) REFERENCES event(eventId), --
+FOREIGN KEY (commentProfileId) REFERENCES profile(profileId), --
+--
+PRIMARY KEY (commentId) --
 );
+
+--The following creates the Event Type table
+
+
+--The following creates the RSVP table
