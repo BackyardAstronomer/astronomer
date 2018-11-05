518f788045b355ce15c1e598c4ae9ed53f90d8e1
Stephen Pelot <spelot@cnm.edu>
2018-11-05 16:04:03 -0700

    Started Comment.php

--- php/Classes/Comment.php
+++ php/Classes/Comment.php
@@ -5,4 +5,26 @@ require_once(dir(__DIR__,) . "/vendor/Autoload.php");
 require_once (".../Classes/Autoload.php");
 use Ramsey\Uuid\Uuid;
 
-class Comment
+/**
+ * Class Comment
+ * @package BackyardAstronomer\astronomer
+ */
+class Comment {
+private $commentId;
+/**
+ * this is the primary key for comment
+ * @var Uuid $commentId;
+ */
+
+private $commentProfileId;
+/**
+ * this is the foreign key of the profile that made the comment
+ * @var Uuid $commentProfileId;
+ */
+
+private $commentEventId;
+/**
+ * this is the foreign key of the event the comment is posted on
+ * @var Uuid $commentEventId
+ */
+}
