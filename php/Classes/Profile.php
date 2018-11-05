cfcd3d94aa02f8e8410ed0a799088c393e0856e1
ChamisaE <43251409+ChamisaE@users.noreply.github.com>
2018-11-05 15:35:20 -0700

    established private variables for Profile class

--- php/Classes/Profile.php
+++ php/Classes/Profile.php
@@ -7,3 +7,53 @@ use Ramsey\Uuid\Uuid;
 /**
  *
  */
+class Profile {
+	use ValidateUuid;
+	use ValidateDate;
+
+	/**
+	 * the following establishes the id for this profile; this is the primary
+	 * key for the table
+	 * @var Uuid $profileId
+	 */
+	private $profileId;
+	/**
+	 * the following establishes the string for the user email.
+	 * @var string $profileEmail
+	 */
+	private $profileEmail;
+	/**
+	 * the following establishes the variable for the null-able text string
+	 *bio the user can choose to make.
+	 * @var string $profileBio
+	 */
+	private $profileBio;
+	/**
+	 * The following establishes the variable for the required name field.
+	 * @var string $profileName
+	 */
+	private $profileName;
+	/**
+	 * the following establishes the variable for the null-able image field.
+	 * @var string $profileImage
+	 */
+	private $profileImage;
+	/**
+	 * the following establishes the variable for the required profile activation
+	 * token each profile needs to create an account.
+	 * @var string $profileActivationToken
+	 */
+	private $profileActivationToken;
+	/**
+	 * the following establishes the variable for the required profile hash that
+	 * stores each profile's password in an encrypted format
+	 * @var string $profileHash
+	 */
+	private $profileHash;
+
+	/**
+	 *
+	 */
+
+
+}
