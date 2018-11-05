40ccbb28b5e4a301de267c5585cb485fbe5d42e2
Stephen Pelot <spelot@cnm.edu>
2018-11-05 15:14:06 -0700

    Added Comment.php

---
+++ php/Classes/Comment.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace BackyardAstronomer\astronomer;
+require_once(dir(__DIR__,) . "/vendor/Autoload.php");
+require_once (".../Classes/Autoload.php");
+use Ramsey\Uuid\Uuid;
+
+class Comment
