37fc417b5e92fbf3e9451b7f8a194812cfbf6434
Dayn Augustson <daugustson@cnm.edu>
2018-11-08 19:14:27 -0700

    https://app.asana.com/0/897247891702387/897247891702393
    PDO rsvp inset in to mySQL

--- php/Classes/Rsvp.php
+++ php/Classes/Rsvp.php
@@ -177,7 +177,24 @@ class Rsvp implements \JsonSerializable {
 		$this->rsvpEventCounter = $newRsvpEventCounter;
 	}
 
+**************************************************************************************************************
+	/**
+	 * inserts Rsvp into mySQL
+	 *
+	 * @param \PDO $pdo PDO connection object
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError if $pdo is not a PDO connection object
+	 **/
+	public function insert(\PDO $pdo) : void {
 
+		// create query template
+		$query = "INSERT INTO rsvp (rsvpId, rsvpProfileId, rsvpEventId, rsvpEventCounter  ) VALUES(:rsvpId, :rsvpProfileId, :rsvpEventId, :rsvpEventCounter )";
+		$statement = $pdo->prepare($query);
+
+		// bind the member variables to the place holders in the template
+		$parameters = ["rsvpId" => $this->rsvpId->getBytes(), "rsvpProfileId" => $this->rsvpProfileId->getBytes(), "rsvpProfileId" => $this->rsvpProfileId->getBytes(), "rsvpEventCounter" => $this->rsvpEventCounter,];
+		$statement->execute($parameters);
+	}
 
 
 
