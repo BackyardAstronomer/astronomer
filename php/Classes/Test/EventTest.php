b0fca4661f09ce5950c62c9ad242136b980083ad
Jack Jain <cirocelot@gmail.com>
2018-11-13 16:20:07 -0700

    added eventTest and namespace; require statements

---
+++ php/Classes/Test/EventTest.php
@@ -0,0 +1,22 @@
+<?php
+namespace BackyardAstronomer\Astronomer;
+use BackyardAstronomer\astronomer\{Event, Profile};
+
+// grab the class under scrutiny
+require_once(dirname(__DIR__) . "/autoload.php");
+
+// grab the uuid generator
+require_once(dirname(__DIR__, 2) . "/lib/uuid.php");
+
+
+/**
+ *  Full PHPUnit test for Event class
+ *
+ * This is a complete PHPUnit test for the Event class. It is complete because *all* MySQL/PDO enabled methods are tested for both invalid and valid inputs.
+ *
+ * @see \BackyardAstronomer\Astronomer\Event
+ * @author Jack Jain <jjain2@cnm.edu>
+ */
+
+
+}
