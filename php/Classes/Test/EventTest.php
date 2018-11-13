ec33fe794e1450ebacf0b233b11560d6d51ea0b7
Jack Jain <cirocelot@gmail.com>
2018-11-13 16:47:38 -0700

    added protected variables

--- php/Classes/Test/EventTest.php
+++ php/Classes/Test/EventTest.php
@@ -18,5 +18,45 @@ require_once(dirname(__DIR__, 2) . "/lib/uuid.php");
  * @author Jack Jain <jjain2@cnm.edu>
  */
 
+class EventTest extends AstronomerTestSetUp {
+	/**
+	 * Profile that created the Event; this is for foreign key relations
+	 *@var Profile profile
+	 */
+	protected $profile = null;
+
+	/**
+	 * valid profile hash to create the profile object to own the test
+	 * @var $VALID_HASH
+	 */
+	protected $VALID_PROFILE_HASH;
+
+	/**
+	 * content of the event
+	 * @var string
+	 */
+	protected $VALID_EVENT_CONTENT = "PHPUnit test passing";
+
+	/**
+	 * content of the updated event
+	 * @var string $VALID_EVENT_CONTENT2
+	 */
+	protected $VALID_EVENT_CONTENT2 = "PHPUnit test still passing";
+
+	/**
+	 * timestamp of the event; this starts as null and is assigned later
+	 * @var \DateTime $VALID_EVENT_START_DATE
+	 */
+	protected $VALID_EVENT_START_DATE = null;
+
+	/**
+	 * timestamp of the end of the event; this starts as null and is assigned later
+	 * @var \DateTime $VALID_EVENT_END_DATE
+	 */
+	protected $VALID_EVENT_END_DATE = null;
+
+	/**
+	 * create dependant objects before running each test
+	 */
 
 }
