36a7d87ef71353b53705f897ffb7db37638bd0bd
Chamisa Edmo <chamisaedmo@yahoo.com>
2018-11-13 10:50:03 -0700

    added tables in astronomer test set up file

--- php/Classes/Test/AstronomerTestSetUp.php
+++ php/Classes/Test/AstronomerTestSetUp.php
@@ -10,6 +10,7 @@ use PHPUnit\DbUnit\Operation\{Composite, Factory, Operation};
 
 // grab the encrypted properties file
 require_once("/etc/apache2/capstone-mysql/cohort22/astronomers");
+require_once ("autoload.php");
 require_once(dirname(__DIR__,3) . "/vendor/autoload.php");
 
 /**
@@ -26,17 +27,14 @@ require_once(dirname(__DIR__,3) . "/vendor/autoload.php");
  *
  * *NOTE*: Tables must be added in the order they were created in step (2).
  *
- * @author DaynAugustson <daugustson@cnm.edu>
  **/
 abstract class AstronomerTestSetUp extends TestCase {
 	use TestCaseTrait;
-
 	/**
 	 * PHPUnit database connection interface
 	 * @var Connection $connection
 	 **/
 	protected $connection = null;
-
 	/**
 	 * assembles the table from the schema and provides it to PHPUnit
 	 *
@@ -44,10 +42,13 @@ abstract class AstronomerTestSetUp extends TestCase {
 	 **/
 	public final function getDataSet() : QueryDataSet {
 		$dataset = new QueryDataSet($this->getConnection());
-
 		// add all the tables for the project here
 		// THESE TABLES *MUST* BE LISTED IN THE SAME ORDER THEY WERE CREATED!!!!
-		$dataset->addTable("EventType");
+		$dataset->addTable("profile");
+		$dataset->addTable("eventType");
+		$dataset->addTable("event");
+		$dataset->addTable("comment");
+		$dataset->addTable("rsvp");
 
 		return($dataset);
 	}
