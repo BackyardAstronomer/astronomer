402b61463bf3ae1ea8bf7d9e6df6924fee78c778
Dayn Augustson <daugustson@cnm.edu>
2018-11-14 13:35:53 -0700

    https://app.asana.com/0/897247891702387/897247891702393 create inserting a valid rsvp and varfying it

--- php/Classes/Test/RsvpTest.php
+++ php/Classes/Test/RsvpTest.php
@@ -45,12 +45,12 @@ class TsvpTest extends TestCase {
 		$this->VALID_PROFILE_HASH = password_hash($password, PASSWORD_ARGON2I, ["time_cost" => 384]);
 
 
-		// create and insert a Profile to own the test Rsvp
-		$this->profile = new Profile(generateUuidV4(), "test@phpunit.de", "I am Groot", "Dilbert", "https://media.giphy.com/media/3og0INyCmHlNylks9O/giphy.gif",Null,  $this->VALID_PROFILE_HASH);
+		// create and insert a Profile to the test Rsvp
+		$this->profile = new Profile(generateUuidV4(), Null, "I am Groot", "test@phpunit.de",$this->VALID_PROFILE_HASH ,"https://media.giphy.com/media/3og0INyCmHlNylks9O/giphy.gif", "Dilbert");
 		$this->profile->insert($this->getPDO());
 
 		// create and insert a Event to test Rsvp
-		$this->event = new Event(generateUuidV4(), generateUuidV4(), generateUuidV4(), "blind star watch party","may the braille be with you","05/04/77", "05/25/77");
+		$this->event = new Event(generateUuidV4(), generateUuidV4(), generateUuidV4(), "blind star watch party","May the braille be with you","05/04/77", "05/25/77");
 
 	}
 
@@ -62,7 +62,15 @@ class TsvpTest extends TestCase {
 		$numRows = $this->getConnection()->getRowCount("rsvp");
 
 		// create a new Rsvp and insert into mySQL
-		$rsvpID
+		$rsvp = new Rsvp($this->profile->getProfileId(), $this->event->getEventId());
+		$rsvp->insert($this->getPDO());
+
+		// grab the data from mySQL and enforce the fields match our expectations
+		$pdoRsvp = Rsvp::getRsvpbyRsvpId($this->getPDO(), $rsvp->getRsvpbyRsvpId());
+		$this->asserEquals($numRows = 1, $this->getConnectoion()->getRowCount("rsvp"));
+		$this->assertEquals($pdoRsvp->getRsvpProfileId(), $this->profile->getProfileId());
+		$this->assertEquals($pdoRsvp->getRsvpEventId(), $this->event->getEventId());
+
 	}
 
 
