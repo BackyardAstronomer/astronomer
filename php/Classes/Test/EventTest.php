2819c38683e3eb98be7ffdb6273ea0c0018a7346
Jack Jain <cirocelot@gmail.com>
2018-11-14 12:06:37 -0700

    added eventType functions and state variable; changed profile to eventType in redundant cases

--- php/Classes/Test/EventTest.php
+++ php/Classes/Test/EventTest.php
@@ -26,6 +26,12 @@ class EventTest extends AstronomerTestSetUp {
 	protected $profile = null;
 
 	/**
+	 * EventType that classifies the event; this is for foriegn key relations
+	 * @var EventType eventType
+	 */
+	protected $eventType = null;
+
+	/**
 	 * valid profile hash to create the profile object to own the test
 	 * @var $VALID_HASH
 	 */
@@ -79,6 +85,9 @@ class EventTest extends AstronomerTestSetUp {
 		//create and insert a profile to own the event
 		$this->profile = new Profile(generateUuidV4(), 'thisisanemail@test.com', "this is a bio blah blah blah blah", "Test Astronomer", null, null, $this->VALID_PROFILE_HASH);
 		$this->profile->insert($this->getPDO());
+		//create and insert an event type to classify the event
+		$this->eventType = new EventType(generateUuidV4(), "This Is Event Name");
+		$this->eventType->insert($this->getPDO());
 
 		//calculate the start date (date unit test was set up...)
 		$this->VALID_EVENT_START_DATE = new \DateTime();
@@ -92,7 +101,7 @@ class EventTest extends AstronomerTestSetUp {
 
 		//create new event and insert into MySQL
 		$eventId = generateUuidV4();
-		$event = new Event($eventId, $this->profile->getProfileId(), $this->profile->getProfileId(), $this->VALID_EVENT_TITLE, $this->VALID_EVENT_CONTENT, $this->VALID_EVENT_START_DATE, $this->VALID_EVENT_END_DATE);
+		$event = new Event($eventId, $this->eventType->getEventTypeId(), $this->profile->getProfileId(), $this->VALID_EVENT_TITLE, $this->VALID_EVENT_CONTENT, $this->VALID_EVENT_START_DATE, $this->VALID_EVENT_END_DATE);
 		$event->insert($this->getPDO());
 
 		//grab the data form MySQL and enforce the fields match our expectations
@@ -118,7 +127,7 @@ class EventTest extends AstronomerTestSetUp {
 
 		//create a new Event and insert into MySQL
 		$eventId = generateUuidV4();
-		$event = new Event($eventId, $this->profile->getProfileId(), $this->profile->getProfileId(), $this->VALID_EVENT_TITLE, $this->VALID_EVENT_CONTENT, $this->VALID_EVENT_START_DATE, $this->VALID_EVENT_END_DATE);
+		$event = new Event($eventId, $this->eventType->getEventTypeId(), $this->profile->getProfileId(), $this->VALID_EVENT_TITLE, $this->VALID_EVENT_CONTENT, $this->VALID_EVENT_START_DATE, $this->VALID_EVENT_END_DATE);
 		$event->insert($this->getPDO());
 
 		//edit the Event and update in MySQL
@@ -129,7 +138,7 @@ class EventTest extends AstronomerTestSetUp {
 		$pdoEvent = Event::getEventByEventId($this->getPDO(), $event->getEventId());
 		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("event"));
 		$this->assertEquals($pdoEvent->getEventId(), $eventId);
-		$this->assertEquals($pdoEvent->getEventEventTypeId(), $this->profile->getProfileId());
+		$this->assertEquals($pdoEvent->getEventEventTypeId(),$this->eventType->getEventTypeId());
 		$this->assertEquals($pdoEvent->getEventProfileId(), $this->profile->getProfileId());
 		$this->assertEquals($pdoEvent->getEventTitle(), $this->VALID_EVENT_TITLE2);
 		$this->assertEquals($pdoEvent->getEventContent(), $this->VALID_EVENT_CONTENT2);
@@ -147,7 +156,7 @@ class EventTest extends AstronomerTestSetUp {
 
 		//create a new Event and insert into MySQL
 		$eventId = generateUuidV4();
-		$event = new Event($eventId, $this->profile->getProfileId(), $this->profile->getProfileId(), $this->VALID_EVENT_TITLE, $this->VALID_EVENT_CONTENT, $this->VALID_EVENT_START_DATE, $this->VALID_EVENT_END_DATE);
+		$event = new Event($eventId, $this->eventType->getEventTypeId(), $this->profile->getProfileId(), $this->VALID_EVENT_TITLE, $this->VALID_EVENT_CONTENT, $this->VALID_EVENT_START_DATE, $this->VALID_EVENT_END_DATE);
 		$event->insert($this->getPDO());
 
 		//delete Event from MySQL
@@ -172,13 +181,50 @@ class EventTest extends AstronomerTestSetUp {
 	/**
 	 * test inserting an Event and re-grabbing it from MySQL
 	 */
+	public function testGetValidEventByEventEventTypeId() {
+		//count number of rows and save for later
+		$numRows = $this->getConnection()->getRowCount("event");
+
+		//create a new Event and insert into MySQL
+		$eventId = generateUuidV4();
+		$event = new Event($eventId, $this->eventType->getEventTypeId(), $this->profile->getProfileId(), $this->VALID_EVENT_TITLE, $this->VALID_EVENT_CONTENT, $this->VALID_EVENT_START_DATE, $this->VALID_EVENT_END_DATE);
+		$event->insert($this->getPDO());
+
+		//grab the data from MySQL and enforce expectations
+		$results = Event::getEventByEventEventTypeId($this->getPDO(), $event->getEventEventTypeId());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("event"));
+		$this->assertCount(1, $results);
+		$this->assertContainsOnlyInstancesOf("BackyardAstronomer\\Astronomer\\php\\Classes\\Event", $results);
+
+		//grab the result from the array and validate it
+		$pdoEvent = $results[0];
+
+		$this->assertEquals($pdoEvent->getEventId(), $eventId);
+		$this->assertEquals($pdoEvent->getEventEventTypeId(), $this->eventType->getEventTypeId());
+		$this->assertEquals($pdoEvent->getEventContent(), $this->VALID_EVENT_CONTENT);
+		//format the dates 2 seconds since the beginning of time to avoid rounding error
+		$this->assertEquals($pdoEvent->getEventStartDate()->getTimestamp(), $this->VALID_EVENT_START_DATE->getTimestamp());
+		$this->assertEquals($pdoEvent->getEventEndDate()->getTimestamp(), $this->VALID_EVENT_END_DATE->getTimestamp());
+	}
+	/**
+	 * test grabbing an Event that does not exist
+	 */
+	public function testGetInvalidEventByEventEventTypeId() : void {
+		//grab a profile id that exceeds the maximum allowable profile id
+		$event = Event::getEventByEventEventTypeId($this->getPDO(), generateUuidV4());
+		$this->assertCount(0, $event);
+	}
+
+	/**
+	 * test inserting an Event and re-grabbing it from MySQL
+	 */
 	public function testGetValidEventByEventProfileId() {
 		//count number of rows and save for later
 		$numRows = $this->getConnection()->getRowCount("event");
 
 		//create a new Event and insert into MySQL
 		$eventId = generateUuidV4();
-		$event = new Event($eventId, $this->profile->getProfileId(), $this->profile->getProfileId(), $this->VALID_EVENT_TITLE, $this->VALID_EVENT_CONTENT, $this->VALID_EVENT_START_DATE, $this->VALID_EVENT_END_DATE);
+		$event = new Event($eventId, $this->eventType->getEventTypeId(), $this->profile->getProfileId(), $this->VALID_EVENT_TITLE, $this->VALID_EVENT_CONTENT, $this->VALID_EVENT_START_DATE, $this->VALID_EVENT_END_DATE);
 		$event->insert($this->getPDO());
 
 		//grab the data from MySQL and enforce expectations
