9c9a76624207fc07cad57bac66e84e8715048f72
Chamisa Edmo <chamisaedmo@yahoo.com>
2018-11-24 14:56:31 -0700

    edits to profile API. https://app.asana.com/0/897247891702387/918455261817254

--- API/profile/profileAPI.php
+++ API/profile/profileAPI.php
@@ -34,8 +34,9 @@ try {
 	$method = $_SERVER["HTTP_X_HTTP_METHOD"] ?? $_SERVER["REQUEST_METHOD"];
 	// sanitize input
 	$id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-	$profileAtHandle = filter_input(INPUT_GET, "profileAtHandle", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	$profileName = filter_input(INPUT_GET, "profileName", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 	$profileEmail = filter_input(INPUT_GET, "profileEmail", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	$profileActivationToken = filter_input(INPUT_GET, "profileActivationToken", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 	// make sure the id is valid for methods that require it
 	if(($method === "DELETE" || $method === "PUT") && (empty($id) === true)) {
 		throw(new InvalidArgumentException("id cannot be empty or negative", 405));
@@ -43,24 +44,30 @@ try {
 	if($method === "GET") {
 		//set XSRF cookie
 		setXsrfCookie();
+
 		//gets a post by content
 		if(empty($id) === false) {
 			$reply->data = Profile::getProfileByProfileId($pdo, $id);
 		} else if(empty($profileAtHandle) === false) {
-			$reply->data = Profile::getProfileByProfileAtHandle($pdo, $profileAtHandle);
+			$reply->data = Profile::getProfileByProfileName($pdo, $profileName);
 		} else if(empty($profileEmail) === false) {
 			$reply->data = Profile::getProfileByProfileEmail($pdo, $profileEmail);
+		} else if(empty($profileActivationToken) === false) {
+			$reply->data = Profile::getProfileByProfileActivationToken($pdo, $profileActivationToken);
 		}
 	} elseif($method === "PUT") {
 		//enforce that the XSRF token is present in the header
 		verifyXsrf();
+
 		//enforce the end user has a JWT token
 		//validateJwtHeader();
+
 		//enforce the user is signed in and only trying to edit their own profile
 		if(empty($_SESSION["profile"]) === true || $_SESSION["profile"]->getProfileId()->toString() !== $id) {
 			throw(new \InvalidArgumentException("You are not allowed to access this profile", 403));
 		}
 		validateJwtHeader();
+
 		//decode the response from the front end
 		$requestContent = file_get_contents("php://input");
 		$requestObject = json_decode($requestContent);
@@ -69,38 +76,44 @@ try {
 		if($profile === null) {
 			throw(new RuntimeException("Profile does not exist", 404));
 		}
-		//profile at handle
-		if(empty($requestObject->profileAtHandle) === true) {
-			throw(new \InvalidArgumentException ("No profile at handle", 405));
+		//profile name
+		if(empty($requestObject->profileName) === true) {
+			throw(new \InvalidArgumentException ("No profile with this name", 405));
 		}
 		//profile email is a required field
 		if(empty($requestObject->profileEmail) === true) {
 			throw(new \InvalidArgumentException ("No profile email present", 405));
 		}
-		//profile phone # | if null use the profile phone that is in the database
-		if(empty($requestObject->profilePhone) === true) {
-			$requestObject->ProfilePhone = $profile->getProfilePhone();
+		//profile activation token| if null use the profile activation token that is in the database
+		if(empty($requestObject->profileActivationToken) === true) {
+			$requestObject->ProfileActivationToken = $profile->getProfileActivationToken();
 		}
-		$profile->setProfileAtHandle($requestObject->profileAtHandle);
+		$profile->setProfileId($requestObject->profileId);
+		$profile->setProfileName($requestObject->profileName);
 		$profile->setProfileEmail($requestObject->profileEmail);
-		$profile->setProfilePhone($requestObject->profilePhone);
+		$profile->setProfileActivationToken($requestObject->profileActivationToken);
 		$profile->update($pdo);
+
 		// update reply
 		$reply->message = "Profile information updated";
 	} elseif($method === "DELETE") {
 		//verify the XSRF Token
 		verifyXsrf();
+
 		//enforce the end user has a JWT token
 		//validateJwtHeader();
+
 		$profile = Profile::getProfileByProfileId($pdo, $id);
 		if($profile === null) {
 			throw (new RuntimeException("Profile does not exist"));
 		}
+
 		//enforce the user is signed in and only trying to edit their own profile
 		if(empty($_SESSION["profile"]) === true || $_SESSION["profile"]->getProfileId()->toString() !== $profile->getProfileId()->toString()) {
 			throw(new \InvalidArgumentException("You are not allowed to access this profile", 403));
 		}
 		validateJwtHeader();
+
 		//delete the post from the database
 		$profile->delete($pdo);
 		$reply->message = "Profile Deleted";
