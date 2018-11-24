8ba615d92848416863563a11cb0be07f25852b30
Jack Jain <cirocelot@gmail.com>
2018-11-24 14:42:39 -0700

    added if(GET) to the api

--- API/event/eventAPI.php
+++ API/event/eventAPI.php
@@ -31,5 +31,36 @@ $reply->data = null;
 
 try {
 	//get mysql connection
-	$pdo = connectToEncryptedMySQL("/etc/apache2?")
+	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/cohort22/astronomers.ini");
+
+	// which http method used
+	$method = $_SERVER["HTTP_X_HTTP_METHOD"] ?? $_SERVER["REQUEST_METHOD"];
+
+	//sanitize input
+	$id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	$eventEventTypeId = filter_input(INPUT_GET, "eventEventTypeId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	$eventProfileId = filter_input(INPUT_GET, "eventProfileId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+
+	//ensure id is valid for methods that need it
+	if(($method === "DELETE" || $method = "PUT") && (empty($id) === true)){
+		throw(new InvalidArgumentException("in cannot be empty or negative", 402));
+	}
+
+	// handle GET request - if id is present, that event is returned, otherwise all events are returned
+	if($method === "GET"){
+		//set xsrf token
+		setXsrfCookie();
+		//get a specific event or all events and update reply
+		if(empty($id) === false) {
+			$reply->data = Event::getEventByEventId($pdo, $id);
+		} else if(empty($eventEventTypeId) === false) {
+			$reply->data = Event::getEventByEventEventTypeId($pdo, $_SESSION["eventType"]->getEventTypeId())->toArray();
+		} else if(empty($eventProfileId) === false) {
+			//if user is logged in grab all events by that user based on log in
+			$reply->data = Event::getEventByEventProfileId($pdo, $_SESSION["profile"]->getProfileId())->toArray();
+		}else{
+			$reply->data = Event::getAllEvents($pdo)->toArray();
+		}
+
+	}
 }
