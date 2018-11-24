465b00373c56a46f63f90c35dee46a6453bdbd68
Jack Jain <cirocelot@gmail.com>
2018-11-24 15:16:42 -0700

    added put and post methods

--- API/event/eventAPI.php
+++ API/event/eventAPI.php
@@ -37,13 +37,13 @@ try {
 	$method = $_SERVER["HTTP_X_HTTP_METHOD"] ?? $_SERVER["REQUEST_METHOD"];
 
 	//sanitize input
-	$id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	$eventId = filter_input(INPUT_GET, "eventId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 	$eventEventTypeId = filter_input(INPUT_GET, "eventEventTypeId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 	$eventProfileId = filter_input(INPUT_GET, "eventProfileId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 
 	//ensure id is valid for methods that need it
-	if(($method === "DELETE" || $method = "PUT") && (empty($id) === true)){
-		throw(new InvalidArgumentException("in cannot be empty or negative", 402));
+	if(($method === "DELETE" || $method = "PUT") && (empty($eventId) === true)){
+		throw(new InvalidArgumentException("id cannot be empty or negative", 401));
 	}
 
 	// handle GET request - if id is present, that event is returned, otherwise all events are returned
@@ -51,8 +51,8 @@ try {
 		//set xsrf token
 		setXsrfCookie();
 		//get a specific event or all events and update reply
-		if(empty($id) === false) {
-			$reply->data = Event::getEventByEventId($pdo, $id);
+		if(empty($eventId) === false) {
+			$reply->data = Event::getEventByEventId($pdo, $eventId);
 		} else if(empty($eventEventTypeId) === false) {
 			$reply->data = Event::getEventByEventEventTypeId($pdo, $_SESSION["eventType"]->getEventTypeId())->toArray();
 		} else if(empty($eventProfileId) === false) {
@@ -61,6 +61,32 @@ try {
 		}else{
 			$reply->data = Event::getAllEvents($pdo)->toArray();
 		}
+	} else if($method ==="PUT" || $method === "POST") {
+		//enforce user has xsrf token
+		verifyXsrf();
+		// enforce user is signed in
+		if(empty($_SESSION["profile"]) === true) {
+			throw(new \InvalidArgumentException("you must be signed in to create events", 401));
+		}
+		$requestContent = file_get_contents("php://input");
+		// retrieves JSON package that the front end sent, and stores it in $requestContent
+		$requestObject = json_decode($requestContent);
+		// this line decodes json package and stores result in $requestObject
+		// ensure event content is available (required)
+		if(empty($requestObject->eventContent) === true) {
+			throw(new \InvalidArgumentException("No content for Event.", 405));
+		}
 
+		// ensure date is not null
+		if(empty($requestObject->eventStartDate) === true) {
+			throw(new \InvalidArgumentException("events must have start dateTime", "https://http.cat/[406].jpeg"));
+		}
+		if(empty($requestObject->eventEndDate) === true) {
+			throw(new \InvalidArgumentException("events must have end dateTime", "https://http.cat/[406].jpeg"));
+		}
+		if($method === "PUT") {
+			// retrieve the event to update
+			$event = Event::
+		}
 	}
 }
