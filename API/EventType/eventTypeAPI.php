3690d674e0f48adfea41a861642c81f421d70187
Dayn Augustson <daugustson@cnm.edu>
2018-11-24 17:51:52 -0700

    doing a get request
    https://app.asana.com/0/897247891702387/897247891702395

--- API/EventType/eventTypeAPI.php
+++ API/EventType/eventTypeAPI.php
@@ -7,8 +7,9 @@ require_once dirname(__DIR__, 3) . "../php/lib/uuid.php";
 require_once dirname(__DIR__, 3) . "../php/lib/jwt.php";
 require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 
-use BackyardAstronomer\Astronomer\EventType;
-
+use BackyardAstronomer\Astronomer\ {
+	EventType
+};
 
 
 /**
@@ -38,8 +39,22 @@ try {
 
 
 	// make sure the id is valid for methods that require it
-	if(($method === "DELETE" || $method === "PUT") && (empty($id) === true)) {
+	if(($method === "DELETE" || $method === "PUT") && (empty($evenTypeId) === true)) {
 		throw(new InvalidArgumentException("id cannot be empty or negative", 405));
 	}
+
+// handle GET request - if evenTypeId is present, that EventType is returned, otherwise all EventType are returned
+	if($method === "GET") {
+		//set XSRF cookie
+		setXsrfCookie();
+		//get a specific Event type or all Event type and update reply
+		if(empty($evenTypeId) === false) {
+			$reply->data = EventType::getEventTypeByEventTypeId($pdo, $evenTypeId);
+		} else if(empty(($evenTypeId) === false) {
+
+		}
+	}
+
+
 
 }
