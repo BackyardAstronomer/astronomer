34b09b25d4a644262496a5abca1700139ff7e50c
Dayn Augustson <daugustson@cnm.edu>
2018-12-02 11:17:49 -0700

    Delete get and eventTypeID
    https://app.asana.com/0/897247891702387/897247891702395

--- public_html/api/EventType/eventTypeAPI.php
+++ public_html/api/EventType/eventTypeAPI.php
@@ -34,25 +34,16 @@ try {
 	$method = $_SERVER["HTTP_X_HTTP_METHOD"] ?? $_SERVER["REQUEST_METHOD"];
 
 // sanitize input
-	$evenTypeId = filter_input(INPUT_GET, "evenTypeId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+
 	$eventTypeName = filter_input(INPUT_GET, "eventTypeName ", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 
 
 	// make sure the id is valid for methods that require it
-	if(($method === "DELETE" || $method === "PUT") && (empty($evenTypeId) === true)) {
+	if(($method === "DELETE" || $method === "PUT") && (empty($evenTypeName) === true)) {
 		throw(new InvalidArgumentException("EventTypeID cannot be empty or negative", 405));
 	}
 
-// handle GET request - if evenTypeId is present, that EventType is returned, otherwise all EventType are returned
-	if($method === "GET") {
 
-		//set XSRF cookie
-		setXsrfCookie();
-		//get a specific Event type or all Event type and update reply
-		if(empty($evenTypeId) === false) {
-			$reply->data = EventType::getEventTypeByEventTypeId($pdo, $evenTypeId);
-		}
-	}
 
 
 
