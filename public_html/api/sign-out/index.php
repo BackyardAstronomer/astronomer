0539b19ac34055ca379984d27f225d91725a1863
Stephen Pelot <spelot@cnm.edu>
2018-12-03 10:22:58 -0700

    cleaned up require once block on sign in api

--- public_html/api/sign-out/index.php
+++ public_html/api/sign-out/index.php
@@ -1,7 +1,11 @@
 <?php
-require_once dirname(__DIR__, 3) . "/php/classes/autoload.php";
+require_once dirname(__DIR__, 3) . "/vendor/autoload.php";
+require_once dirname(__DIR__, 3) . "/php/Classes/autoload.php";
 require_once dirname(__DIR__, 3) . "/php/lib/xsrf.php";
-require_once ("/etc/apache2/capstone-mysql/encrypted-config.php");
+require_once dirname(__DIR__, 3) . "/php/lib/uuid.php";
+require_once("/etc/apache2/capstone-mysql/Secrets.php");
+
+
 /**
  * api for signing out
  *
