7e9a7d8ce5a7c41a08fc710579e8fed7339ef1ce
Jack Jain <cirocelot@gmail.com>
2018-12-03 08:56:47 -0700

    completed activation api; ready for testing
    Signed-off-by: Jack Jain <cirocelot@gmail.com>

--- public_html/api/activation/index.php
+++ public_html/api/activation/index.php
@@ -3,7 +3,7 @@ require_once dirname(__DIR__,3)."php/classes/autoload.php";
 require_once dirname(__DIR__,3)."php/lib/xsrf.php";
 require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 
-use BackyardAstronomer\Astronomer;
+use BackyardAstronomer\Astronomer\Profile;
 /**
  * API to check profile activation status
  * @author Gkephart
@@ -13,4 +13,72 @@ if(session_status() !== PHP_SESSION_ACTIVE){
 	session_start();
 }
 
-//prepare and empty reply
+//prepare an empty reply
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
+try{
+	//grab MySQL connection object
+	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/cohort22/astronomers.ini");
+
+	//check HTTP method being used
+	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
+
+	//sanitize input (because end users are always malicious)
+	$activation = filter_input(INPUT_GET, "activation", FILTER_SANITIZE_STRING);
+
+	//make sure the activation token is the correct size
+	if(strlen($activation) !== 32){
+		throw(new InvalidArgumentException("activation has an incorrect length", 405));
+	}
+
+	//verify activation token is a string value in hexadecimal
+	if(ctype_xdigit($activation) === false) {
+		throw(new \InvalidArgumentException("activation is empty or has invalid content", 405));
+	}
+	//handle the GET HTTP request
+	if($method === "GET"){
+
+		//set XSRF cookie
+		setXsrfCookie();
+
+		//find profile associated with the activation token
+		$profile = Profile::getProfileByProfileActivationToken($pdo, $activation);
+
+		//verify the profile is not null
+		if($profile !== null){
+
+			//make sure activation token matches
+			if($activation === $profile->getProfileActivationToken()) {
+
+				//set activation to null
+				$profile->setProfileActivationToken(null);
+
+				//update profile in the database
+				$profile->update($pdo);
+
+				//set the reply for the end user
+				$reply->data = "Thank you for activating your account, you will be auto-redirected to your profile shortly.";
+			}
+		}else {
+			//throw an exceprion if the activation token does not exist
+			throw(new RuntimeException("Profile with this activation code does not exist", 404));
+		}
+	}else {
+		//throw an exception if HTTP request is not a GET
+		throw(new InvalidArgumentException("Invalid HTTP method request", 403));
+	}
+} catch(Exception $exception){
+	$reply->status = $exception->getCode();
+	$reply->message = $exception->getMessage();
+}catch(TypeError $typeError){
+	$reply->status = $typeError->getCode();
+	$reply->message = $typeError->getMessage();
+}
+
+//prepare and send the reply
+header("Content-type: application/json");
+if($reply->data === null){
+	unset($reply->data);
+}
+echo json_encode($reply);
