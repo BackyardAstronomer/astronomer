fc566818d3232052e35bf52ec1aa592c75a6642a
Stephen Pelot <spelot@cnm.edu>
2018-12-04 15:01:34 -0700

    got "post" to work in comment api

--- public_html/api/comment/index.php
+++ public_html/api/comment/index.php
@@ -83,6 +83,7 @@ try {
 		//enforce that the user has an XSRF token
 		verifyXsrf();
 
+
 		$requestContent = file_get_contents("php://input");
 		// Retrieves the JSON package that the front end sent, and stores it in $requestContent. Here we are using file_get_contents("php://input") to get the request from the front end. file_get_contents() is a PHP function that reads a file into a string. The argument for the function, here, is "php://input". This is a read only stream that allows raw data to be read from the front end request which is, in this case, a JSON package.
 		$requestObject = json_decode($requestContent);
