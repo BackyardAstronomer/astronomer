b3da17332a72452a1cdf4a5266491326f3a2f85c
Dayn Augustson <daugustson@cnm.edu>
2018-12-04 15:39:02 -0700

    create-account fixed some typos

--- static-ui/create-account.php
+++ static-ui/create-account.php
@@ -8,7 +8,7 @@
 				<input type="text" class="form-control" id="profileName" name="name" placeholder="User Name">
 				<input type="email" class="form-control" id="profileEmail" name="email" placeholder="Email">
 				<input type="password" class="form-control" id="profilePassword" name="password" placeholder="Password">
-				<input type="password" class="form-control" id="profilePasswordConfirm" name="password-confirm" placeholder="Re-enter Password">
+				<input type="password" class="form-control" id="profilePasswordConfirm" name="password-confirm " placeholder="Re-enter Password">
 				<input class="btn" type="submit" value="Sign Up!">
 			</div>
 		</form>
