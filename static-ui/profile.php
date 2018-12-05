0e76b173883d8ed259f51ed4cf554be8d28b98fd
Stephen Pelot <spelot@cnm.edu>
2018-12-05 10:09:28 -0700

    added card to profile.php static ui, still considering using something else

--- static-ui/profile.php
+++ static-ui/profile.php
@@ -2,10 +2,21 @@
 <html>
 	<?php
 	require_once ("head-utils.php");
+	require_once ("navbar.php")
 	?>
 	<body>
-		<?php
-		require_once
-		?>
+		<div class="card">
+			<div class="col col-3">
+			<img class="card-img-top" src="/images/pathToYourImage.png" alt="Card image cap">
+			<div class="card-body">
+				<h4 class="card-title">Card title</h4>
+				<p class="card-text">
+					Some quick example text to build on the card title
+					and make up the bulk of the card's content.
+				</p>
+				<a href="#!" class="btn btn-primary">Go somewhere</a>
+			</div>
+			</div>
+		</div>
 	</body>
 </html>
