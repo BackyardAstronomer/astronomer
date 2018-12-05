4d9e9d5892ed2eae0b451094b63840dccc4fee23
Chamisa Edmo <chamisaedmo@yahoo.com>
2018-12-05 14:38:31 -0700

    still trying to figure out how to separate the background photo from the navbar

--- static-ui/template.php
+++ static-ui/template.php
@@ -5,15 +5,20 @@
 	require_once ("navbar.php");
 	require_once ("head-utils.php");
 	?>
+<div class="header">
 
+</div>
 	<!--link to CSS-->
 	<link rel="stylesheet" href="style.css"/>
 
 </head>
-<body
-	<div>
+<body>
+		<div class="container">
+			<div class="jumbotron">
 	<!--link to background image-->
 	<img src="img/sky.jpg" id="bg" alt="galaxy">
+			</div>
+		</div>
 	</div>
 </body>
 </html>
