03b79ff4c7da8af43d9a79c90e6d6ec1a38423e3
Dayn Augustson <daugustson@cnm.edu>
2018-12-04 19:06:37 -0700

    created navbar

--- static-ui/navbar.php
+++ static-ui/navbar.php
@@ -1,3 +1,27 @@
-<header>
-	
-</header>
+<?php require_once ("head-utils.php");?>
+
+<!-- This sets up my nav bar grabed this code from getbootstrap.com -->
+		<header>
+			<nav class="navbar navbar-expand-lg">
+				<a class="navbar-brand" href="#"></a>
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+
+				<div class="collapse navbar-collapse" id="navbarSupportedContent">
+					<ul class="navbar-nav ml-auto">
+						<li class="nav-item">
+							<a class="nav-link" href="#">Event</a>
+						<li class="nav-item">
+							<a class="nav-link" href="#">ToBeNamed</a>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Log in</a>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Create Account</a>
+						</li>
+					</ul>
+				</div>
+			</nav>
+		</header>
+
+
