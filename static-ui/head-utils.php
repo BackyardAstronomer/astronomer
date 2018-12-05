b21e437583812023d3193bd7e62d1d3384adcb1e
Jack Jain <cirocelot@gmail.com>
2018-12-05 10:10:54 -0700

    added link in head-utils.php in case george lets me import those nice cdn's
    Signed-off-by: Jack Jain <cirocelot@gmail.com>

--- static-ui/head-utils.php
+++ static-ui/head-utils.php
@@ -19,5 +19,7 @@
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
 
+	<!--Tempus Dominus CDN (for the dateTimePicker PENDING GEORGE'S APPROVAL  (https://tempusdominus.github.io/bootstrap-4/)-->
+
 	<title>ABQ Astronomers</title>
 </head>
