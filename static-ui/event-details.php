c4f2b9bbd0e17a179a217c45f825dcc88f68a423
Jack Jain <cirocelot@gmail.com>
2018-12-05 10:22:01 -0700

    completed event creation form
    Signed-off-by: Jack Jain <cirocelot@gmail.com>

--- static-ui/event-details.php
+++ static-ui/event-details.php
@@ -11,7 +11,10 @@
 					<textarea rows="5" class="form-control" id="eventContent" placeholder="Write a short description of the event here."></textarea>
 				</div>
 				<div class="form-group col-8-offset-2">
-
+					<input type="datetime-local" class="form-control" id="startDateTime" placeholder="select the starting date and time">
+				</div>
+				<div class="form-group col-8-offset-2">
+					<input type="datetime-local" class="form-control" id="endDateTime" placeholder="select the expected end date and time">
 				</div>
 			</form>
 		</div>
