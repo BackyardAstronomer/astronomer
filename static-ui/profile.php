e55cfb954e0f6296fb53bbef1630a5c2786096d1
Stephen Pelot <spelot@cnm.edu>
2018-12-05 13:20:49 -0700

    Decided on a card format for the profile (everything else looks awful) and am currently trying a card format for the event scrolling

--- static-ui/profile.php
+++ static-ui/profile.php
@@ -1,22 +1,52 @@
 <!DOCTYPE html>
 <html>
 	<?php
-	require_once ("head-utils.php");
-	require_once ("navbar.php")
+	require_once("head-utils.php");
+	require_once("navbar.php")
 	?>
 	<body>
-		<div class="card">
-			<div class="col col-3">
-			<img class="card-img-top" src="/images/pathToYourImage.png" alt="Card image cap">
-			<div class="card-body">
-				<h4 class="card-title">Card title</h4>
-				<p class="card-text">
-					Some quick example text to build on the card title
-					and make up the bulk of the card's content.
-				</p>
-				<a href="#!" class="btn btn-primary">Go somewhere</a>
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-4">
+					<div class="card">
+
+						<img class="card-img-top" src="/images/pathToYourImage.png" alt="Profile Picture">
+						<div class="card-body">
+							<h4 class="card-title">Profile Name</h4>
+							<p class="card-text">
+								Email
+							</p>
+							<p class="card-text">
+								Bio
+							</p>
+							<p class="card-text">
+
+							</p>
+						</div>
+					</div>
 			</div>
-			</div>
-		</div>
+
+
+
+				<div class="row">
+					<div class="col-8">
+						<div class="card">
+
+							<img class="card-img-top" src="/images/pathToYourImage.png" alt="Event Picture">
+							<div class="card-body">
+								<h4 class="card-title">Event Title</h4>
+								<p class="card-text">
+									Event Content
+								</p>
+								<p class="card-text">
+									Event Profile/Host
+								</p>
+								<p class="card-text">
+									Event Start-Event End
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
 	</body>
 </html>
