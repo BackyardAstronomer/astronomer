d39101f469b8488666a685eea3871c0210b0c3ee
Jack Jain <cirocelot@gmail.com>
2018-12-05 09:59:44 -0700

    getting ready for new cdn's to include the datetimepicker
    Signed-off-by: Jack Jain <cirocelot@gmail.com>

--- static-ui/event-details.php
+++ static-ui/event-details.php
@@ -4,13 +4,13 @@
 		<h1>Create Event</h1>
 		<div class="row justify-content-center">
 			<form>
-				<div class="form-group col-4">
+				<div class="form-group col-8-offset-2">
 					<input type="text" class="form-control" id="eventTitle" placeholder="Title of event.">
 				</div>
-				<div class="form-group col-4">
+				<div class="form-group col-8-offset-2">
 					<textarea rows="5" class="form-control" id="eventContent" placeholder="Write a short description of the event here."></textarea>
 				</div>
-				<div class="form-group col-4">
+				<div class="form-group col-8-offset-2">
 
 				</div>
 			</form>
