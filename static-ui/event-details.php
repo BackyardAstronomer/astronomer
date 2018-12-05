c1dd42637cc8858f3fe83dc56accb1db9a780341
Jack Jain <cirocelot@gmail.com>
2018-12-05 09:11:44 -0700

    added event-details.php form structure
    Signed-off-by: Jack Jain <cirocelot@gmail.com>

---
+++ static-ui/event-details.php
@@ -0,0 +1,16 @@
+<?php require_once ("head-utils.php");?>
+<main>
+	<div class="container text-center">
+		<h1>Create Event</h1>
+		<div class="row justify-content-center">
+			<form>
+				<div class="form-group col-4">
+					<input type="text" class="form-control" id="eventTitle" placeholder="Title of event">
+				</div>
+				<div class="form-group">
+					<input type="text">
+				</div>
+			</form>
+		</div>
+	</div>
+</main>
