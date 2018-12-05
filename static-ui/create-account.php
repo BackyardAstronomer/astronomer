491187907e37719f5ed772d1df591547b6cde5ff
Dayn Augustson <daugustson@cnm.edu>
2018-12-04 17:01:21 -0700

    create-account fixed some typos

--- static-ui/create-account.php
+++ static-ui/create-account.php
@@ -1,6 +1,7 @@
 <?php require_once ("head-utils.php");?>
 
 <main>
+
 	<div class="container text-center">
 		<h1>Sign Up for Backyard Astronomer!</h1>
 		<form class="form-control-lg" id="form" action="" method="post">
@@ -8,9 +9,10 @@
 				<input type="text" class="form-control" id="profileName" name="name" placeholder="User Name">
 				<input type="email" class="form-control" id="profileEmail" name="email" placeholder="Email">
 				<input type="password" class="form-control" id="profilePassword" name="password" placeholder="Password">
-				<input type="password" class="form-control" id="profilePasswordConfirm" name="password-confirm " placeholder="Re-enter Password">
+				<input type="password" class="form-control" id="profilePasswordConfirm" name="password-confirm" placeholder="Re-enter Password">
 				<input class="btn" type="submit" value="Sign Up!">
 			</div>
 		</form>
 	</div>
+
 </main>
