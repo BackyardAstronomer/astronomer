0e34a6d3ef271f19f1d9d9f1c44836cdbfd8bd35
Chamisa Edmo <chamisaedmo@yahoo.com>
2018-12-06 15:10:35 -0700

    added create-account.php html to splash component

--- static-ui/splash-page.php
+++ static-ui/splash-page.php
@@ -1,24 +1,22 @@
 <!DOCTYPE html>
-<html>
+<html xmlns:https="http://www.w3.org/1999/xhtml">
 	<head>
 	<?php require_once ("head-utils.php")?>
 	<?php require_once("navbar.php")?>
 	</head>
 
 	<body>
+
 		<!-- You can now use your library component in app.component.html -->
-		<ngx-slick class="carousel" #slickModal="slick-modal" [config]="slideConfig" (afterChange)="afterChange($event)">
-			<div ngxSlickItem *ngFor="let slide of slides" class="slide">
-				<div class="card" style="width: 18rem;">
-					<img src="{{ slide.img }}" alt="" width="100%">
+				<div class="card" style="width: 21.6rem;">
+					<img src="" alt="" width="100%">
 					<div class="card-body">
-						<img src="{{star.png}}">
+						<img src="https://placeimg.com/640/480/any" width="300px" height="300px">
 						<h5 class="card-title">Card title</h5>
 						<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-						<a href="#" class="btn btn-primary">Go somewhere</a>
+						<a href="#" class="btn btn-primary">Click for more information!</a>
 					</div>
 				</div>
 			</div>
-		</ngx-slick>
 	</body>
 </html>
