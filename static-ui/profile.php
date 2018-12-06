183ef32b29b3913293656d267087dca00c019c3e
Stephen Pelot <spelot@cnm.edu>
2018-12-06 13:47:56 -0700

    Trying a different format for the scrolling events on the profile page

--- static-ui/profile.php
+++ static-ui/profile.php
@@ -24,29 +24,13 @@
 							</p>
 						</div>
 					</div>
-			</div>
-
-
-
-				<div class="row">
-					<div class="col-8">
-						<div class="card">
-
-							<img class="card-img-top" src="/images/pathToYourImage.png" alt="Event Picture">
-							<div class="card-body">
-								<h4 class="card-title">Event Title</h4>
-								<p class="card-text">
-									Event Content
-								</p>
-								<p class="card-text">
-									Event Profile/Host
-								</p>
-								<p class="card-text">
-									Event Start-Event End
-								</p>
-							</div>
-						</div>
-					</div>
 				</div>
+			</div>
+			<div class="d-flex flex-column">
+				<div class="p-2">Flex item 1</div>
+				<div class="p-2">Flex item 2</div>
+				<div class="p-2">Flex item 3</div>
+			</div>
+		</div>
 	</body>
 </html>
