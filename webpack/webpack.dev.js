072f0021bb608f411a5be79fa70d1ecbac9debac
Chamisa Edmo <chamisaedmo@yahoo.com>
2018-12-07 08:42:59 -0700

    deleted target.js added targetUrl.js

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -3,7 +3,7 @@ var webpackMerge = require("webpack-merge");
 var MiniCssExtractPlugin = require("mini-css-extract-plugin");
 var commonConfig = require("./webpack.common.js");
 var helpers = require("./helpers");
-var targetUrl = require("./target.js");
+var targetUrl = require("./targetUrl.js");
 
 const ENV = process.env.NODE_ENV = process.env.ENV = "dev";
 
