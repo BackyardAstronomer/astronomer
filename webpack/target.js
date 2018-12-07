ba4a5aa982a746d39e7cdfa10e992e8274ef7741
Dayn Augustson <daugustson@cnm.edu>
2018-12-06 18:22:27 -0700

    added target to fgitoinore

--- webpack/target.js
+++ webpack/target.js
@@ -1,4 +1,4 @@
 module.exports = function() {
 	//path to your deployment API directory
-	return basepath = "https://bootcamp-coders.cnm.edu/~cedmo/astronomer/public_html/"; //Your URL here
+	return basepath = "https://bootcamp-coders.cnm.edu/~dauguston/astronomer/public_html/"; //Your URL here
 };
