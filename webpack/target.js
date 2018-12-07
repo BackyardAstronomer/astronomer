5ab9aed8be103b15b138e1a282ad66c6aa074bf9
Stephen Pelot <spelot@cnm.edu>
2018-12-06 18:37:23 -0700

    it was chamisa's fault

--- webpack/target.js
+++ webpack/target.js
@@ -1,4 +1,4 @@
 module.exports = function() {
 	//path to your deployment API directory
-	return basepath = "https://bootcamp-coders.cnm.edu/~daugustson/astronomer/public_html/"; //Your URL here
+	return basepath = "https://bootcamp-coders.cnm.edu/~spelot/astronomer/public_html/"; //Your URL here
 };
