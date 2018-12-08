633b37809302f8dff417b79157c5ca44b4f4e427
Stephen Pelot <spelot@cnm.edu>
2018-12-08 12:41:00 -0700

    cleaned up id tags for the forms on edit-profile.php

--- static-ui/edit-profile.php
+++ static-ui/edit-profile.php
@@ -5,10 +5,11 @@
 	<form>
 		<div class="container text-center" id="edit-profile">
 			<h1>Edit your Profile</h1>
-			<input type="text" class="form-control" id="exampleFormControlInput1" placeholder="New Username">
-			<input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Update Email">
-			<input type="password" class="form-control" id="exampleFormControlInput1" placeholder="Enter New Password">
-			<textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Enter Bio Here"></textarea>
+			<input type="text" class="form-control" id="profileName" name="Username" placeholder="New Username">
+			<input type="email" class="form-control" id="profileEmail" name="Email" placeholder="Update Email">
+			<input type="password" class="form-control" id="profilePassword" name="Password" placeholder="Enter New Password">
+			<input type="password" class="form-control" id="profilePasswordConfirm" name="Password" placeholder="Confirm Password">
+			<textarea class="form-control" id="profileBio" rows="3" placeholder="Enter Bio Here"></textarea>
 			<br>
 			<input class="btn" type="submit" value="Save your changes!">
 		</div>
