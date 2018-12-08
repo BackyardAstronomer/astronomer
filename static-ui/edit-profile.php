28c95e5185d980dd46c3216fda257c8f9b7a6c66
Stephen Pelot <spelot@cnm.edu>
2018-12-08 11:31:11 -0700

    edit-profile.php matches the other form pages now

--- static-ui/edit-profile.php
+++ static-ui/edit-profile.php
@@ -3,21 +3,11 @@
 
 <form>
 	<form>
-		<div class="form-group">
-			<label for="exampleFormControlInput1">Username</label>
+		<div class="container text-center" id="edit-profile">
+			<h1>Edit your Profile</h1>
 			<input type="text" class="form-control" id="exampleFormControlInput1" placeholder="New Username">
-		</div>
-		<div class="form-group">
-			<label for="exampleFormControlInput1">Email address</label>
 			<input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Update Email">
-		</div>
-		<div class="form-group">
-			<label for="exampleFormControlInput1">Password</label>
 			<input type="password" class="form-control" id="exampleFormControlInput1" placeholder="Enter New Password">
-		</div>
-
-		<div class="form-group">
-			<label for="exampleFormControlTextarea1">Bio</label>
 			<textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Enter Bio Here"></textarea>
 		</div>
 	</form>
