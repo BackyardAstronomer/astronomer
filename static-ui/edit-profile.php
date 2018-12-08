d7d68f1d85c93f174d152125fb15ac7dfcdfbe94
Stephen Pelot <spelot@cnm.edu>
2018-12-07 18:23:50 -0700

    beginning of the forms on edit-profile.php
    The four forms are there, just need some cleaning up

--- static-ui/edit-profile.php
+++ static-ui/edit-profile.php
@@ -1,2 +1,22 @@
 <?php require_once ("head-utils.php")?>
 <?php require_once ("navbar.php");?>
+
+<form>
+	<div class="form-group">
+		<label for="exampleInputEmail1">Profile Name</label>
+		<input type="name" class="form-control" id="example" aria-describedby="emailHelp" placeholder="Enter Username">
+	</div>
+	<div class="form-group">
+		<label for="exampleInputPassword1">Email</label>
+		<input type="Email" class="form-control" id="exampleInputPassword1" placeholder="Enter Email">
+	</div>
+	<div class="form-group">
+		<label for="exampleInputPassword1">Bio</label>
+		<input type="Bio" class="form-control" id="exampleInputPassword1" placeholder="Enter Bio">
+	</div>
+	<div class="form-group">
+		<label for="exampleInputPassword1">Password</label>
+		<input type="Password" class="form-control" id="exampleInputPassword1" placeholder="Enter Password">
+	</div>
+	<button type="submit" class="btn btn-primary">Submit</button>
+</form>
