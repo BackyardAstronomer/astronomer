fef54d0dd33e63c2d689387bf1bf63660d8f9efd
Stephen Pelot <spelot@cnm.edu>
2018-12-08 11:58:24 -0700

    events are correctly aligned to the right in edit-profile.php

--- static-ui/profile.php
+++ static-ui/profile.php
@@ -5,7 +5,7 @@
 <main>
 <div class="container-fluid">
 			<div class= "sticky-top">
-				<div class="col-4 offset-4">
+				<div class="col-4">
 					<div class="card">
 
 						<img class="card-img-top" src="./img/placeholder.jpg" alt="Profile Picture">
@@ -18,27 +18,24 @@
 								Bio
 							</p>
 							<p class="card-text">
-
 							</p>
 						</div>
 					</div>
 				</div>
 			</div>
-			<div class="container-fluid">
-				<div class="row">
-					<div class="jumbotron col-8 offset-2">
+					<div class="jumbotron col-8 offset-4">
 						<div class="container">
 							<h1 class="display-4">Event 1</h1>
 							<p class="lead">This is where the information pertaining to the event will be shown. It includes date, time, details about the event's purpose, and the location where it will be held.</p>
 						</div>
 					</div>
-					<div class="jumbotron col-8 offset-2">
+					<div class="jumbotron col-8 offset-4">
 						<div class="container">
 							<h1 class="display-4">Event 2</h1>
 							<p class="lead">This is where the information pertaining to the event will be shown. It includes date, time, details about the event's purpose, and the location where it will be held.</p>
 						</div>
 					</div>
-					<div class="jumbotron col-8 offset-2">
+					<div class="jumbotron col-8 offset-4">
 						<div class="container">
 							<h1 class="display-4">Event 3</h1>
 							<p class="lead">This is where the information pertaining to the event will be shown. It includes date, time, details about the event's purpose, and the location where it will be held.</p>
@@ -47,4 +44,5 @@
 				</div>
 			</div>
 		</div>
-	</main>
+	</div>
+</main>
