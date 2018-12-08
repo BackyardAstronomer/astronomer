e29d812a59a6117fb8f99d9c98b07d89a7f4b5a6
Stephen Pelot <spelot@cnm.edu>
2018-12-08 12:31:24 -0700

    sticky works, and it should be good to go

--- static-ui/profile.php
+++ static-ui/profile.php
@@ -5,7 +5,8 @@
 <main>
 	<div class="container-fluid">
 		<div class="row">
-				<div class="col-4 sticky-top">
+				<div class="col-4">
+					<div class="sticky-top">
 
 					<div class="card">
 
@@ -23,6 +24,7 @@
 						</div>
 					</div>
 			</div>
+				</div>
 				<div class="col-8">
 					<div class="jumbotron">
 						<div class="container">
@@ -44,19 +46,19 @@
 					</div>
 					<div class="jumbotron">
 						<div class="container">
-							<h1 class="display-4">Event 3</h1>
+							<h1 class="display-4">Event 4</h1>
 							<p class="lead">This is where the information pertaining to the event will be shown. It includes date, time, details about the event's purpose, and the location where it will be held.</p>
 						</div>
 					</div>
 					<div class="jumbotron">
 						<div class="container">
-							<h1 class="display-4">Event 3</h1>
+							<h1 class="display-4">Event 5</h1>
 							<p class="lead">This is where the information pertaining to the event will be shown. It includes date, time, details about the event's purpose, and the location where it will be held.</p>
 						</div>
 					</div>
 					<div class="jumbotron">
 						<div class="container">
-							<h1 class="display-4">Event 3</h1>
+							<h1 class="display-4">Event 6</h1>
 							<p class="lead">This is where the information pertaining to the event will be shown. It includes date, time, details about the event's purpose, and the location where it will be held.</p>
 						</div>
 					</div>
