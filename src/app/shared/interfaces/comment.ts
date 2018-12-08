0b162e7767389a3d11401fc690da9699c7b3ad53
Chamisa Edmo <chamisaedmo@yahoo.com>
2018-12-08 16:00:07 -0700

    added comment and event interfaces

---
+++ src/app/shared/interfaces/comment.ts
@@ -0,0 +1,7 @@
+export interface Comment{
+commentId: string;
+commentProfileId: string;
+commentEventId: string;
+commentContent: string;
+commentDate: Date;
+}
---
+++ src/app/shared/interfaces/event.ts
@@ -0,0 +1,8 @@
+export interface Event{
+	eventEventTypeId: string;
+	eventProfileId: string;
+	eventTitle: string;
+	eventContent: string;
+	eventStartDate: Date;
+	eventEndDate: Date;
+}
