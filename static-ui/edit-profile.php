8b408f891eab7c51a8682cf12321c988d258d110
Stephen Pelot <spelot@cnm.edu>
2018-12-08 11:25:01 -0700

    changed forms a bit to match their types better

--- static-ui/edit-profile.php
+++ static-ui/edit-profile.php
@@ -2,21 +2,23 @@
 <?php require_once ("navbar.php");?>
 
 <form>
-	<div class="form-group">
-		<label for="exampleInputEmail1">Profile Name</label>
-		<input type="name" class="form-control" id="example" aria-describedby="emailHelp" placeholder="Enter Username">
-	</div>
-	<div class="form-group">
-		<label for="exampleInputPassword1">Email</label>
-		<input type="Email" class="form-control" id="exampleInputPassword1" placeholder="Enter Email">
-	</div>
-	<div class="form-group">
-		<label for="exampleInputPassword1">Bio</label>
-		<input type="Bio" class="form-control" id="exampleInputPassword1" placeholder="Enter Bio">
-	</div>
-	<div class="form-group">
-		<label for="exampleInputPassword1">Password</label>
-		<input type="Password" class="form-control" id="exampleInputPassword1" placeholder="Enter Password">
-	</div>
-	<button type="submit" class="btn btn-primary">Submit</button>
+	<form>
+		<div class="form-group">
+			<label for="exampleFormControlInput1">Username</label>
+			<input type="text" class="form-control" id="exampleFormControlInput1" placeholder="New Username">
+		</div>
+		<div class="form-group">
+			<label for="exampleFormControlInput1">Email address</label>
+			<input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Update Email">
+		</div>
+		<div class="form-group">
+			<label for="exampleFormControlInput1">Password</label>
+			<input type="password" class="form-control" id="exampleFormControlInput1" placeholder="Enter New Password">
+		</div>
+
+		<div class="form-group">
+			<label for="exampleFormControlTextarea1">Bio</label>
+			<textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Enter Bio Here"></textarea>
+		</div>
+	</form>
 </form>
