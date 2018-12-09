ffdcc12a54dec2f5bf373b7b074a8a48b8df7533
Chamisa Edmo <chamisaedmo@yahoo.com>
2018-12-09 12:55:43 -0700

    added buncha directories and files inside those

--- static-ui/sign-in.php
+++ static-ui/sign-in.php
@@ -2,28 +2,17 @@
 <?php require_once ("navbar.php");?>
 
 <main>
-	<!-- Modal -->
-	<div class="modal fade" id="signIn" tabindex="-1" role="dialog" aria-labelledby="signIn" aria-hidden="true">
-		<div class="modal-dialog" role="document">
-			<div class="modal-content">
-				<div class="modal-header">
-					<h5 class="modal-title" id="signIn">Sign In</h5>
-					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-						<span aria-hidden="true">&times;</span>
-					</button>
-				</div>
-				<div class="modal-body">
-					<form class="form-control-lg" id="form" action="" method="post">
-						<div class="info">
-							<input type="email" class="form-control" id="profileEmail"  name="email" placeholder="Email">
-							<input type="text" class="form-control" id="ProfilePassword" name="password" placeholder="Password">
-						</div>
-					</form>
-					<div class="modal-footer">
-						<input class="btn" type="submit" value="Sign In!">
-					</div>
-				</div>
+
+	<div id="signin" class="container text-center">
+		<h1>Sign into Backyard Astronomer!</h1>
+		<form class="form-control-lg" id="form" action="" method="post">
+			<div class="info">
+				<input type="email" class="form-control" id="profileEmail" name="email" placeholder="Email">
+				<input type="password" class="form-control" id="profilePassword" name="password" placeholder="Password">
+				<input type="password" class="form-control" id="profilePasswordConfirm" name="password-confirm" placeholder="Re-enter Password">
+				<br>
+				<input class="btn" type="submit" value="Sign In!">
 			</div>
-		</div>
+		</form>
 	</div>
 </main>
