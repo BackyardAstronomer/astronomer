5e64ccb4a6cfddec3918e79eb138e0c5ca338e6c
Chamisa Edmo <chamisaedmo@yahoo.com>
2018-12-09 10:47:37 -0700

    more profiles for database

--- php/lib/mock-data.php
+++ php/lib/mock-data.php
@@ -1,20 +1,28 @@
 <?php
-namespace BackyardAstronomer\Astronomer\Profile;
+use BackyardAstronomer\Astronomer\{Profile, Event, EventType, Comment, Rsvp};
 
 // grab the class under scrutiny
-require_once(dirname(__DIR__) . "/classes/autoload.php");
-require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
+require_once(dirname(__DIR__, 1) . "/Classes/autoload.php");
+require_once("/etc/apache2/capstone-mysql/Secrets.php");
 require_once("uuid.php");
 
 // grab the uuid generator
 require_once( "uuid.php");
-$pdo =  new \Secrets("/etc/apache2/capstone-mysql/.ini");
+$pdo =  new \Secrets("/etc/apache2/capstone-mysql/cohort22/astronomers");
+$pdo = $pdo->getPdoObject();
 
 	$hash = password_hash("abc123", PASSWORD_ARGON2I, ["time_cost" => 384]);
 
-$profile = new Profile(generateUuidV4(), null,  null, "amanda@nasa.com", $hash, null, "Amanda");
-echo "Amanda's profile : $profile->getProfileId()";
+//$profile = new Profile(generateUuidV4(), null,  null, "amanda@nasa.com", $hash, null, "Amanda");
+//$profile->insert($pdo);
+//echo "Amanda's profile : $profile->getProfileId()";
 
-$profile2 = new Profile(generateUuidV4(), null,  null, "amanda@nasa.com", $hash, null, "John");
+$profile2 = new Profile(generateUuidV4(), null,  null, "john@nasa.com", $hash, null, "John");
+$profile2->insert($pdo);
+echo "johns's profile : " . $profile2->getProfileId();
 
-$profile = new Profile(generateUuidV4(), null, null, "amanda@nasa.com", $hash, null, "Jacob");
+$profile3 = new Profile(generateUuidV4(), null, null, "jacob@nasa.com", $hash, null, "Jacob");
+$profile3->insert($pdo);
+echo "jacob's profile : " . $profile3->getProfileId();
+
+$event = new Event(id)
