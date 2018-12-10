0f64ee619d84534a4f5a4afc5e9d17d45f1b1788
Jack Jain <cirocelot@gmail.com>
2018-12-10 10:33:16 -0700

    styling event-details static ui, added placeholder image for card

--- static-ui/event-details.php
+++ static-ui/event-details.php
@@ -4,18 +4,15 @@
 
 
 <main>
-	<!--This is where the JUMBOTRON is located-->
-	<div class="container">
-		<div class="jumbotron container">
-			<h2 class="display-3 text-center">Event Name!</h2>
-			<p class="lead">This is a simple hero unit, a simple jumbotron-style component for
-				calling extra attention to featured content or information.</p>
-			<hr class="my-2">
-			<p>It uses utility classes for typography and
-				spacing to space content out within the larger container.</p>
-			<p class="lead">
-				<a class="btn btn-primary btn-lg" href="#!" role="button">RSVP</a>
-			</p>
+<div class="container justify-content-center">
+	<div class="row ">
+		<div class="card col-12 my-5 justify-content-center">
+			<img class="card-image-top" src="./img/planet-placeholders.jpg" alt="Planet cartoon image">
+			<div class="card-body">
+				<h5 class="card-title">Event</h5>
+
+			</div>
 		</div>
 	</div>
+</div>
 </main>
