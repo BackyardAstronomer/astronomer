605484c512be03ef80d653db13db3ea317c5868f
Jack Jain <cirocelot@gmail.com>
2018-12-10 11:01:41 -0700

    Fixed image sizing and added description of event

--- static-ui/event-details.php
+++ static-ui/event-details.php
@@ -6,11 +6,14 @@
 <main>
 <div class="container justify-content-center">
 	<div class="row ">
-		<div class="card col-12 my-5 justify-content-center">
-			<img class="card-image-top" src="./img/planet-placeholders.jpg" alt="Planet cartoon image">
+		<div class="card col-12 my-5 px-0 justify-content-center">
+			<img class="card-img-top" src="./img/planet-placeholders.jpg" alt="Planet cartoon image">
 			<div class="card-body">
-				<h5 class="card-title">Event</h5>
+				<h2 class="card-title">Event</h2>
+				<p class="card-text">Here is a short description of the event.</p>
+				<div>
 
+				</div>
 			</div>
 		</div>
 	</div>
