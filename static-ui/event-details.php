34fea7bbf7c76a72063b62a842df2ef8f7cec7fc
Dayn Augustson <daugustson@cnm.edu>
2018-12-10 09:06:28 -0700

    testing out differnt looks for Event details

--- static-ui/event-details.php
+++ static-ui/event-details.php
@@ -1,12 +1,21 @@
 <?php require_once ("head-utils.php")?>
-
 <?php require_once ("navbar.php");?>
 
 
+
 <main>
-	<div class="jumbotron">
-		<div class="container">
-			<
+	<!--This is where the JUMBOTRON is located-->
+	<div class="container">
+		<div class="jumbotron container">
+			<h2 class="display-3 text-center">Event Name!</h2>
+			<p class="lead">This is a simple hero unit, a simple jumbotron-style component for
+				calling extra attention to featured content or information.</p>
+			<hr class="my-2">
+			<p>It uses utility classes for typography and
+				spacing to space content out within the larger container.</p>
+			<p class="lead">
+				<a class="btn btn-primary btn-lg" href="#!" role="button">RSVP</a>
+			</p>
 		</div>
 	</div>
 </main>
